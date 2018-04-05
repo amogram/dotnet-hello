e4bce936d7de6152d81135421110aff96d51c4e9
Andrew McCaughan <andrew.mccaughan@gmail.com>
2018-04-05 21:17:16 +0100

    Added the code

---
+++ dotnet-hello/FunctionHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text;
+
+namespace Function
+{
+    public class FunctionHandler
+    {
+        public void Handle(string input) {
+            Console.WriteLine("Hi from dotnetcore. You've entered: " + input + ". This was kindly executed on " + Environment.MachineName);
+        }
+    }
+}
